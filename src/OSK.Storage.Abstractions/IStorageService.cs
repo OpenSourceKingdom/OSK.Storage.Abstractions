260a65e786cb557d46b69b4c3b566281c69993d6
BlankDev117 <mmmasent@gmail.com>
2024-03-26 00:30:21 -0400

    Need initial project (OSK-1) (#2)
    Motivation
    ----
    * Added project
    * Added github workflows
    
    Result
    ----
    An initial project containing storage Abstractions as well as github workflows is available

---
+++ src/OSK.Storage.Abstractions/IStorageService.cs
@@ -0,0 +1,17 @@
+﻿using OSK.Functions.Outputs.Abstractions;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlankStudios.Common.Storage.Abstractions
+{
+    public interface IStorageService<TSaveOptions, TSearchOptions>
+        where TSaveOptions: class
+        where TSearchOptions : class
+    {
+        Task<IOutput<StorageMetaData>> SaveAsync<T>(T data, string fullStoragePath, TSaveOptions options = null, CancellationToken cancellationToken = default);
+        Task<IOutput<StorageObject>> GetAsync(string fullStoragePath, CancellationToken cancellationToken = default);
+        Task<IOutput<IEnumerable<StorageMetaData>>> GetStorageDetailsAsync(string directoryPath, TSearchOptions options = null, CancellationToken cancellationToken = default);
+        Task<IOutput> DeleteAsync(string fullStoragePath, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/OSK.Storage.Abstractions/StorageMetaData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace BlankStudios.Common.Storage.Abstractions
+{
+    public class StorageMetaData
+    {
+        #region Variables
+
+        public string FileName => Path.GetFileNameWithoutExtension(FullStoragePath);
+        public string StorageDirectory => Path.GetDirectoryName(FullStoragePath);
+        public string Extension => Path.GetExtension(FullStoragePath);
+
+        public string FullStoragePath { get; }
+
+        public bool IsEncrypted { get; }
+
+        public long ContentSize { get; }
+
+        public string StorageMimeType { get; internal set; }
+
+        public DateTime LastModifiedTimeUtc { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public StorageMetaData(string storagePath, long contentSize, bool isEncrypted,
+            string mimeType, DateTime dateTimeUtcSinceLastUpdateTime)
+        {
+            FullStoragePath = storagePath;
+            ContentSize = contentSize;
+            IsEncrypted = isEncrypted;
+            StorageMimeType = mimeType;
+            LastModifiedTimeUtc = dateTimeUtcSinceLastUpdateTime;
+        }
+
+        #endregion
+    }
+}
